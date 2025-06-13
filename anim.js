ddc69d39ecc62fcbb3b90a86a42e8b75844ca8ae
Sayee Anand <sayeea37@gmail.com>
2025-06-13 14:46:37 -0500

    made basic blog esque layout, click to continue now works.

--- anim.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('splash').addEventListener('click', function () {
-    document.getElementById('splash').style.display = 'none';
-    document.getElementById('main-content').classList.remove('hidden');
-});
